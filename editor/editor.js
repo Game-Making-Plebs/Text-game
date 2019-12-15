18adca8f521bfb93cf745e2bdd7e71c836084dd5
Veeti Karttunen <veeti.karttunen@gmail.com>
2019-12-15 03:27:18 +0200

    Start working on World editor

--- editor/editor.js
+++ editor/editor.js
@@ -1,3 +1,16 @@
-jsPlumb.bind("ready", function(){
+const jsp = require('jsplumb');
+let nonPlumbing = {};
+let elementCounter = 0;
 
+jsPlumb.bind("ready", function(){
+    jsPlumb.setContainer("canvas");
+    nonPlumbing = jsPlumb.getInstance();
 });
+
+loadFile = () => {
+    fetch("http://localhost/test.json")
+        .then(response => response.text())
+        .then((data) => {
+            console.log(data)
+        })
+}
